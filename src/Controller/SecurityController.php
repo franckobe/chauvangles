d79980056e7868e17858029f11f61e5ac3bf2f94
Honayme <honayme@gmail.com>
2018-10-25 12:14:17 +0200

    [Login/Register/Logout]

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\Registration;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -19,6 +23,40 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig',
+            ['last_username' => $lastUsername,
+                'error' => $error]);
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncode){
+
+        $user = new User();
+        $form = $this->createForm(Registration::class, $user);
+        $controller_name = "SecurityController";
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password
+            $password = $passwordEncode->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            // 4) save the User
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render(
+            'security/register.html.twig',
+            array('form' => $form->createView(),
+                'controller_name' => $controller_name,
+                )
+        );
     }
 }
