8177444ff4c73f707614b22a50b7354fa97e9bfb
Honayme <honayme@gmail.com>
2018-11-15 15:03:04 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/franckobe/chauvangles""
    This reverts commit 46582a3e825c000d2e0bf290be20d14f78ff4980.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -40,6 +40,12 @@ class User implements UserInterface
      */
     private $password;
 
+    /**
+     * @var boolean the status of the User 1 for connected 0 for disconnected
+     * @ORM\Column(type="boolean")
+     */
+    private $status;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -53,6 +59,18 @@ class User implements UserInterface
     public function setEmail(string $email): self
     {
         $this->email = $email;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status): self
+    {
+        $this->status = $status;
 
         return $this;
     }
