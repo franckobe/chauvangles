acc21d057572512c5ae0b72058976bcee70e4acb
Honayme <honayme@gmail.com>
2018-12-02 16:32:23 +0100

    [Members] Get all function is now clean

--- src/Controller/MembersController.php
+++ src/Controller/MembersController.php
@@ -26,37 +26,36 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class MembersController extends AbstractController
 {
-
-    //@Route("/restapi/members/get-all", name="members_all")
     /**
-     * @Route("/members/get-all", name="members_all")
+     * @Route("/restapi/members/get-all", name="members_all")
      * @return \Symfony\Component\HttpFoundation\JsonResponse|Response
      */
     public function members_all(SerializerInterface $serializer): Response
     {
-        $controller_name="error";
-        $error = "E0003";
-        $description_error="Bail non renouvelable";
+        $controller_name = (string) null;
+        $code = (string) null;
+        $description = (string) null;
+        $request_token = (string) null;
+        $jsonDecode = (string) null;
 
-        if ($this->denyAccessUnlessGranted('IS_AUTHENTICATED_ANONYMOUSLY')) {
-            // get the login error if there is one
-            $error = $authenticationUtils->getLastAuthenticationError();
-            // last username entered by the user
-            $lastUsername = $authenticationUtils->getLastUsername();
+        //Get User request
+        $request_str = $this->container->get('request_stack')->getCurrentRequest()->getContent(); //STRING
+        $request_json = json_decode($request_str, true); //object JSON
 
-            return $this->render('security/login.html.twig',
-                ['last_username' => $lastUsername,
-                    'error' => $error]);
+        if($request_json){
+            foreach ($request_json as $key => $value){
+                if ($key === 'token') {
+                    $request_token = $value;
+                }
+            }
         }
-        else
-        {
-            $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-            $controller_name = "members";
-            $code = "T0004";
-            $description = "Liste des utilisateurs inscrits";
+
+        if($request_token !== null && $request_token === $this->getUser()->getApiToken()) {
+            $controller_name = 'members';
+            $code = 'T0004';
+            $description = 'Liste des utilisateurs inscrits';
 
             //-------------FETCH RESULTs---------------------------------------------------
-
             $users = $this->getDoctrine()
                 ->getRepository(User::class)
                 ->findAll();
@@ -66,24 +65,27 @@ class MembersController extends AbstractController
                 $users,
                 'json', array('groups' => array('group1', 'group2'))
             );
-
             $jsonDecode = json_decode($json);
 
-            //Mise en forme du contenu --------
-            $resp_content_json = array(
-                'type' => $controller_name,
-                'code' => $code,
-                'description' => $description,
-                'payload' => $jsonDecode
-            );
-
-            $resp_jwt = JWT::encode($resp_content_json,'toto');          //On le met au format JWT
-            $resp_jwt_json = $this->json(array(
-                'jwt'=> $resp_jwt
-            ));
-
-            return $resp_jwt_json;
+        }else {
+            $controller_name='error';
+            $code = 'E0003';
+            $description='Bail non renouvelable';
         }
+
+        //Mise en forme du contenu --------
+        $resp_content_json = array(
+            'type' => $controller_name,
+            'code' => $code,
+            'description' => $description,
+            'payload' => $jsonDecode
+        );
+
+        $resp_jwt_json = $this->json(array(
+            'jwt'=> JWT::encode($resp_content_json,getenv('APP_SECRET'))
+        ));
+
+        return $resp_jwt_json;
     }
 
     /**
@@ -101,9 +103,11 @@ class MembersController extends AbstractController
         $request_str = $this->container->get('request_stack')->getCurrentRequest()->getContent(); //STRING
         $request_json = json_decode($request_str, true); //object JSON
 
-        foreach ($request_json as $key => $value){
-            if ($key === 'token') {
-                $request_token = $value;
+        if($request_json) {
+            foreach ($request_json as $key => $value) {
+                if ($key === 'token') {
+                    $request_token = $value;
+                }
             }
         }
 
